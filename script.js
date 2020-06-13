add0ec6690dfe148afdc3a12f9c4d417bdf1a99b
Sam Dratch <dratch.sam1@gmail.com>
2020-06-13 17:59:32 -0400

    added files and psuedo code

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+console.log(moment().format("dddd MMMM Do"));
+
+//display date
+//get current time
+//add new divs with classes to sheet
+//change div color based on time
+//save form input to local storage when save button is complete

b707c22589e5bd2338e00a1031b156c51247f5be
Sam Dratch <dratch.sam1@gmail.com>
2020-06-13 21:57:56 -0400

    Change color based on time

--- script.js
+++ script.js
@@ -3,23 +3,16 @@
 //console.log(moment().format("dddd MMMM Do"));
 //display date
 $("#currentDay").text(moment().format("dddd[,] MMMM Do"))
+
 //get current time
-
-console.log(moment().hour());
 var currentHour = moment().hour();
-
+//currentHour = 12;
 //add new divs with classes to sheet
 
 for(var i = 9; i<18;i++){
     var workHour = i
-
-    if(i>12){
-        workHour = workHour -12
-        workHour = workHour +"PM"
-    }
-    else{
-        workHour = workHour +"AM"
-    }
+    console.log(workHour)
+    
 //check time for hour and change class for the text
 var timeSlot = $("<div>");
 timeSlot.addClass("row")
@@ -29,8 +22,30 @@ var hourDisplay = $("<div>");
 var textArea = $("<textarea>");
 var saveBttn = $("<button>");
 hourDisplay.addClass("col-1 hour")
-textArea.addClass("col-10 description future")
+textArea.addClass("col-10 description")
+
+if(workHour<currentHour){
+    textArea.addClass("past")
+    console.log(workHour)
+}
+else if(workHour===currentHour){
+    textArea.addClass("present")
+    console.log(workHour)
+}
+else{
+    textArea.addClass("future")
+    console.log(workHour)
+}
+
 saveBttn.addClass("col-1 saveBtn")
+
+if(i>12){
+    workHour = workHour - 12
+    workHour = workHour +"PM"
+}
+else{
+    workHour = workHour +"AM"
+}
 
 hourDisplay.text(workHour)
 saveBttn.html("<i class = 'fas fa-save'></i>")
